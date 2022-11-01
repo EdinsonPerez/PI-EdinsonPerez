f6a5a55b1c63f396fa610bc9a5d43b5607bdba5d
EdinsonPerez <perezea1010@gmail.com>
2022-11-01 14:46:28 -0300

    01-11 api

--- api/src/routes/temperaments.js
+++ api/src/routes/temperaments.js
@@ -3,14 +3,35 @@ const router = Router();
 const {Temperaments} = require('../db');
 
 
+// router.get('/', async (req, res, next) => {
+//     try {
+//         const temperament = await Temperaments.findAll()
+//         res.send(temperament)
+//     } catch(error){
+//         next(error)
+// }
+// })
+
 router.get('/', async (req, res, next) => {
     try {
-        const temperament = await Temperaments.findAll()
-        res.send(temperament)
-    } catch(error){
-        next(error)
-}
-})
+        const temperamentApi = await axios.get('https://api.thedogapi.com/v1/breeds')
+      console.log(temperamentApi)
+        const tem = temperamentApi.data.map(el => el.temperament);
+        console.log(tem)
+      const tempEach = tem.toString().split(",");
+        console.log(tempEach)
+        tempEach.forEach(el => {
+             Temperaments.findOrCreate({
+              where: { name: el }
+          })
+      })
+      const allTemps = await Temperaments.findAll();
+      res.send(allTemps); 
+         
+  }catch(error){
+    next(error)
+  }
+  })
 
 router.post('/', (req, res, next) => {
     const {name} = req.body
